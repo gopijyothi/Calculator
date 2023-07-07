49026275aa8827197a0718a3d192275b28f4bfa1
gopijyothi <138871920+gopijyothi@users.noreply.github.com>
2023-07-07 16:08:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var clickSound=new Audio()
+var clickSound1=new Audio()
+clickSound.src="./click.mp3"
+clickSound1.src="./equal.mp3"
+function clickAudio()
+{
+    clickSound.play()
+}
+function clickAudio1()
+{
+    clickSound1.play()
+}
